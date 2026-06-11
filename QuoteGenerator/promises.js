f7e2890c92847bfb4e86dc44d98cb7c350b50d11
dCoder08 <ddc61623@gmail.com>
2026-06-11 18:10:59 +0530

    Add files via upload

---
+++ QuoteGenerator/promises.js
@@ -0,0 +1,12 @@
+let user={
+    "name" :  "Deepak",
+    "age":20,
+    "class":"se",
+    "addres":{
+        "city":"pune",
+        "locality":"pimpri"
+    }
+}
+
+const user1=JSON.stringify(user);
+console.log(user1);
---
+++ QuoteGenerator/script.js
@@ -0,0 +1,49 @@
+// import { GoogleGenAI } from "@google/genai";
+import { GoogleGenAI } from "https://esm.run/@google/genai";
+
+const ai = new GoogleGenAI({ apiKey: "AQ.Ab8RN6IlvQ1NK3SSoYs8GY9MF5pGOEtIEM4pdJ-S0mHZWv27bQ" });
+
+let cquote;
+let quote;
+let author;
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-3.1-flash-lite",
+    contents: "Generate a  quote and with its author and strictly  make it unique. Provide your response as a valid JSON object with exactly two keys: 'quote' (the quote text) and 'author' (the person who said it, or 'Unknown'). Do not wrap the JSON in markdown code blocks ",
+    
+  });
+  
+
+  let data=JSON.parse(response.text);
+  console.log(data);
+
+  quote=document.getElementById("quote");
+ author=document.getElementById("author");
+cquote=data.quote;
+
+quote.textContent=`${data.quote}`;
+author.textContent=`By~ ${data.author}`;
+
+
+
+}
+
+
+main();
+
+let nextbtn=document.getElementById("next-btn");
+nextbtn.addEventListener('click',()=>{
+ main();
+
+ 
+});
+
+let cpybtn=document.getElementById("copy-btn");
+cpybtn.addEventListener('click',()=>{
+    navigator.clipboard.writeText(cquote);
+    cpybtn.innerText="! copied";
+    setTimeout(() => {
+        cpybtn.innerText="Copy";
+    }, 555);
+})
