92fc417bb935c2396155a3cb242350b419cc9aa5
dCoder08 <ddc61623@gmail.com>
2026-06-14 18:40:05 +0530

    Add files via upload

---
+++ WeatherApp/script.js
@@ -0,0 +1,14 @@
+let input=document.querySelector('input');
+let searchBtn=document.getElementById("search-btn");
+let cityplace=document.getElementById("cityName");
+let temp=document.getElementById("temp");
+let date=document.getElementById("date");
+let time=document.getElementById("time");
+
+
+input.addEventListener('keydown',(e)=>{
+    if(e.key=='Enter')
+        input.value="";
+    fetch
+  
+});
